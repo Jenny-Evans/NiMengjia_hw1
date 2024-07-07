403e39c619b23ed9c719b381bf088e0deba35c15
Jenny-Evans <152413493+Jenny-Evans@users.noreply.github.com>
2024-07-07 22:40:54 +0800

    Update algebra.c

--- src/algebra.c
+++ src/algebra.c
@@ -2,43 +2,88 @@
 #include <stdio.h>
 #include <math.h>
 
-Matrix create_matrix(int row, int col)
-{
+Matrix create_matrix(int row, int col) {
     Matrix m;
     m.rows = row;
     m.cols = col;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            m.data[i][j] = 0.0; 
+        }
+    }
     return m;
 }
 
-Matrix add_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+
+Matrix add_matrix(Matrix a, Matrix b) {
+    if (a.rows != b.rows || a.cols != b.cols) {
+        fprintf(stderr, "Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0); 
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] + b.data[i][j];
+        }
+    }
+    return result;
 }
 
-Matrix sub_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+
+Matrix sub_matrix(Matrix a, Matrix b) {
+    if (a.rows != b.rows || a.cols != b.cols) {
+        fprintf(stderr, "Error: Matrix a and b must have the same rows and cols.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] - b.data[i][j];
+        }
+    }
+    return result;
 }
 
-Matrix mul_matrix(Matrix a, Matrix b)
-{
-    // ToDo
-    return create_matrix(0, 0);
+
+Matrix mul_matrix(Matrix a, Matrix b) {
+    if (a.cols != b.rows) {
+        fprintf(stderr, "Error: The number of cols of matrix a must be equal to the number of rows of matrix b.\n");
+        return create_matrix(0, 0);
+    }
+    Matrix result = create_matrix(a.rows, b.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < b.cols; j++) {
+            result.data[i][j] = 0.0;
+            for (int k = 0; k < a.cols; k++) {
+                result.data[i][j] += a.data[i][k] * b.data[k][j];
+            }
+        }
+    }
+    return result;
 }
 
-Matrix scale_matrix(Matrix a, double k)
-{
-    // ToDo
-    return create_matrix(0, 0);
+
+Matrix scale_matrix(Matrix a, double k) {
+    Matrix result = create_matrix(a.rows, a.cols);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[i][j] = a.data[i][j] * k;
+        }
+    }
+    return result;
 }
 
-Matrix transpose_matrix(Matrix a)
-{
-    // ToDo
-    return create_matrix(0, 0);
+
+Matrix transpose_matrix(Matrix a) {
+    Matrix result = create_matrix(a.cols, a.rows);
+    for (int i = 0; i < a.rows; i++) {
+        for (int j = 0; j < a.cols; j++) {
+            result.data[j][i] = a.data[i][j];
+        }
+    }
+    return result;
 }
+
 
 double det_matrix(Matrix a)
 {
